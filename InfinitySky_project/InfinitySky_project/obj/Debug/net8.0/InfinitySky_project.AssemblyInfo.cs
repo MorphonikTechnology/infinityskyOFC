bd54c8190825c8980af6cea8d9997966e1edc2ad
Eloá Vasconcelos de Macedo <eloavasconcelosds@gmail.com>
2024-09-18 20:13:44 -0300

    Layout da tela home

--- InfinitySky_project/InfinitySky_project/obj/Debug/net8.0/InfinitySky_project.AssemblyInfo.cs
+++ InfinitySky_project/InfinitySky_project/obj/Debug/net8.0/InfinitySky_project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InfinitySky_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a33338a2cc2672b2cf2bf67ea7108df0c83e390")]
 [assembly: System.Reflection.AssemblyProductAttribute("InfinitySky_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InfinitySky_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
